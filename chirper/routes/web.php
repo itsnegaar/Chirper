c835dfa3e9cddc3b122a7dd2c508d742e52755e6
negar.a <negaarabolhasani@gmail.com>
2026-05-10 14:42:00 +0330

    feat: add authentication login and register

--- chirper/routes/web.php
+++ chirper/routes/web.php
@@ -2,9 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ChirpController;
+use App\Http\Controllers\Auth\Register;
 
 Route::get('/', [ChirpController::class, 'index']);
 Route::post('/chirps', [ChirpController::class, 'store']);
 Route::get('/chirps/{chirp}/edit', [ChirpController::class, 'edit']);
 Route::put('/chirps/{chirp}', [ChirpController::class, 'update']);
 Route::delete('/chirps/{chirp}', [ChirpController::class, 'destroy']);
+
+# Register Route
+Route::view('/register', 'auth.register')
+        ->name('register')
+        ->middleware('guest');
+Route::post('/register', Register::class)
+        ->middleware('guest');
